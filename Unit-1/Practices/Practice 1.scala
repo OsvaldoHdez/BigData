7693d0c3d18492d037802a9748bca2d863b35362
uriel2196 <60412166+uriel2196@users.noreply.github.com>
2021-03-18 18:13:45 -0700

    Update Practice 1.scala

--- Unit-1/Practices/Practice 1.scala
+++ Unit-1/Practices/Practice 1.scala
@@ -23,10 +23,21 @@ val bird = "tweet"
 println(s"I'm writing a $bird")
 
 //4. Given the variable message = "Hello Luke, I am your father!" uses slilce to extract the sequence "Luke"
+ scala> val mensaje = "Hello Luke I am your father!"
+ mensaje: String = Hola Luke yo soy tu padre!
 
+ scala> mensaje.slice (5,9)
+ res2: String = Luke
 
 //5. What is the difference between value and a variable in scala?
+ The variable val can NOT be modified once its value has been predefined.
 
-
+The variable var YES its values can be overwritten, it can be modified. (It can only be overwritten if the data to be entered is the same type of data as the previous one)
+ 
 //6. Given the tuple (2,4,5,1,2,3,3.1416,23) returns the number 3.1416
 
+scala> val tupla = (2,4,5,1,2,3,3.1416,23)
+tupla: (Int, Int, Int, Int, Int, Int, Double, Int) = (2,4,5,1,2,3,3.1416,23)
+
+scala> tupla._7
+res25: Double = 3.1416
