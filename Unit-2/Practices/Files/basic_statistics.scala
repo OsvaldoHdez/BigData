c63f1fb23106f72a7aa605bf4c0a30e99cb2681a
uriel2196 <60412166+uriel2196@users.noreply.github.com>
2021-04-29 16:46:22 -0700

    Update basic_statistics.scala

--- Unit-2/Practices/Files/basic_statistics.scala
+++ Unit-2/Practices/Files/basic_statistics.scala
@@ -70,3 +70,38 @@ val df = data.toDF("label", "features")
 // Se toman los primeros valores del dataframe
 val chi = ChiSquareTest.test(df, "features", "label").head
 
+rintln(s"pValues = ${chi.getAs[Vector](0)}")
+//Despues se buscaran los grados de libertad del modelo
+println(s"degreesOfFreedom ${chi.getSeq[Int](1).mkString("[", ",", "]")}")
+//por ultimo se extraeran ciertos valores de un vector dererminado todo en base a la funcion chi cuadrado
+println(s"statistics ${chi.getAs[Vector](2)}")
+
+
+//Summarizer
+
+//importacion de librerias necesarias, en este uso de vectores y el propio summarizer
+import spark.implicits._    
+import Summarizer._
+
+//se crea un conjunto de vectores o secuencia
+val data = Seq(
+  (Vectors.dense(2.0, 3.0, 5.0), 1.0),
+  (Vectors.dense(4.0, 6.0, 7.0), 2.0)
+)
+
+//Creacion del dataframe a partir de los vectores
+val df = data.toDF("features", "weight")
+
+
+//se hace uso de la libreria summarizer para obtener la media y la varianza de algunos datos en el dataframe solicitado
+val (meanVal, varianceVal) = df.select(metrics("mean", "variance").summary($"features", $"weight").as("summary")).select("summary.mean", "summary.variance").as[(Vector, Vector)].first()
+
+//se imprimen las variables trabajadas anteriormente
+println(s"with weight: mean = ${meanVal}, variance = ${varianceVal}")
+
+//se repite el procesos con 2 nuevas variables 
+val (meanVal2, varianceVal2) = df.select(mean($"features"), variance($"features"))
+  .as[(Vector, Vector)].first()
+// impresión de variables
+println(s"without weight: mean = ${meanVal2}, sum = ${varianceVal2}")
+
